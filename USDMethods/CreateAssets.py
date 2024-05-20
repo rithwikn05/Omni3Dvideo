d090bbc69959e647164c6af5bc2edd09fcdcee12
Rithwik Nukala <rithwikn05@gmail.com>
2024-05-19 17:26:56 -0700

    saving CreateAssets.py changes

---
+++ USDMethods/CreateAssets.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from pxr import Usd
+
+layer_path = str(Path.home() / "defining_prims.usda")
+stage = Usd.Stage.CreateNew(layer_path)
+
+#Creating basic geometry
+def create_cube(str):
+    stage.DefinePrim(str, "Cube")
+    stage.Save()
+
+def create_plane(str):
+    stage.DefinePrim(str, "Plane")
+    stage.Save()
+
+def create_sphere(str):
+    stage.DefinePrim(str, "Sphere")
+    stage.Save()
+
+def get_cube_from_prim_path():  #General example for how we can get prim from path
+    cube: Usd.Prim = stage.DefinePrim(str, "Cube")   #Here we are using a cube object
+    cube_path = cube.GetPath()   #Gets the path of the cube that we created above
+    box: Usd.Prim = stage.GetPrimAthPath(cube_path)  #Get the prim at the specified path
+    print(box)  #prints the prim object at the path that is specified
+
+#Importing assets
+def add_reference(str):
--- USDMethods/File&Stage.py
+++ USDMethods/File&Stage.py
@@ -3,5 +3,6 @@ from pxr import Usd
 
 def load_stage(str):
     stage = Usd.Stage.Open(str) #This is the stage object from which we can add prims to the stage
+    stage.Save()
 def save_stage_as(str):
     Usd.Stage.Save(str)     #Loads the stage to the specified file path
