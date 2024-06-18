6eca0a3220815933300ce7f02b4d570ef5574312
DigitalBotLab <yizhou_zhao@berkeley.edu>
2024-06-18 16:21:11 -0700

    add material generator

--- Omni3DVideoExt/exts/omni.3d.video/omni/3d/video/UsdMethods/Material.py
+++ Omni3DVideoExt/exts/omni.3d.video/omni/3d/video/UsdMethods/Material.py
@@ -1,8 +1,67 @@
 from pathlib import Path
-from pxr import Usd, Sdf
+from pxr import Usd, Sdf, UsdShade
+import uuid
+import logging
 
-layer_path = str(Path.home() / "defining_prims.usda")
-stage = Usd.Stage.CreateNew(layer_path)
+import omni.usd
+
+logger = logging.getLogger(__name__)
+
+def generate_texture(prim_path: str, text: str = "A chubby orange cat riding through space, digital art") -> None:
+    """
+    Generate a texture for a prim
+    
+    Args:
+        prim_path (str): the path of the prim to create
+        text (str): the description of the texture
+    """
+
+    from ..GenAI.image_generator import ImageGenerator
+    image_generator = ImageGenerator()
+
+    from ..env import NGC_API_KEY
+    invoke_url = "https://ai.api.nvidia.com/v1/genai/stabilityai/stable-diffusion-xl"
+    image_generator.set_invoke_url(invoke_url)
+    image_generator.set_api_key(NGC_API_KEY)
+    image_generator.set_headers()
+    image_generator.set_payload(prompt=text)
+
+    from ..utils import get_extension_path
+    output_image_folder = get_extension_path() / "output"
+
+    # check if the output folder exists, if not, create it
+    if not output_image_folder.exists():
+        output_image_folder.mkdir()
+
+    # generate image
+    image_generator.run_image_generation()
+    image_random_name = str(uuid.uuid4())[:8]
+    image_store_path = image_generator.save_image(output_image_folder, f"{image_random_name}.png")
+
+    logger.info(f"Image stored at: {image_store_path}")
+    
+    # bind material and texture
+    stage = omni.usd.get_context().get_stage()
+    mtl_random_name = str(uuid.uuid4())[:3]
+    mtl_path = Sdf.Path(f"/World/Looks/GenAI_OmniPBR_{mtl_random_name}")
+    mtl = UsdShade.Material.Define(stage, mtl_path)
+    shader = UsdShade.Shader.Define(stage, mtl_path.AppendPath("Shader"))
+    shader.CreateImplementationSourceAttr(UsdShade.Tokens.sourceAsset)
+    shader.SetSourceAsset("OmniPBR.mdl", "mdl")
+    shader.SetSourceAssetSubIdentifier("OmniPBR", "mdl")
+    mtl.CreateSurfaceOutput("mdl").ConnectToSource(shader.ConnectableAPI(), "out")
+    mtl.CreateDisplacementOutput("mdl").ConnectToSource(shader.ConnectableAPI(), "out")
+    mtl.CreateVolumeOutput("mdl").ConnectToSource(shader.ConnectableAPI(), "out")
+
+    # set the texture
+    diffuse_texture_in = shader.CreateInput("diffuse_texture", Sdf.ValueTypeNames.Asset)
+    diffuse_texture_in.Set(str(image_store_path))
+    diffuse_texture_in.GetAttr().SetColorSpace("sRGB")
+
+    # bind the material to the prim
+    UsdShade.MaterialBindingAPI(stage.GetPrimAtPath(prim_path)).Bind(mtl, UsdShade.Tokens.strongerThanDescendants)
+
+    
 
 #Not really too sure about this one
 class materialAndTexture:
